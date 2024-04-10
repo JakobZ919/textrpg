d654863d97a80990de0e95705be127b2401309c3
Jakob Zmudzinski <140855933+JakobZ919@users.noreply.github.com>
2024-04-10 21:02:07 +0200

    Aktualisieren von main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Player:
     def __init__(self):
-        self.playername = input("What's your name?  ")
+        self.playername = input("What's your name?:")
         self.playerclass = ""
         self.damage = 0
         self.intelligence = 0
